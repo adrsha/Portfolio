09c14b29fb37a5f6879366cadcfab0781fa40be0
Adarsha <friendlybigyan@gmail.com>
2022-09-01 07:37:57 +0545

    Polish

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
     multiplier:0.5,
+    firefoxMultiplier: 25,
     mobile: {
       breakpoint:0,
       smooth: true
@@ -12,3 +13,14 @@ const scroll = new LocomotiveScroll({
     },
     lerp: 0.07,
   });
+
+const projectLink =  document.querySelector('.project-a');
+const scrollDown =  document.querySelector('span.scroll-down');
+const projectDestination =  document.querySelector('.projects');
+
+document.addEventListener('click', (e)=>{
+  if (e.target == projectLink || e.target == scrollDown){
+    console.log("This worked");
+    scroll.scrollTo(projectDestination);
+  }
+})
